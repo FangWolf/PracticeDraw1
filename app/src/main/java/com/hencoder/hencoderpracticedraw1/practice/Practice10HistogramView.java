ef9a1f4c501953bdcc70abdb74cfa723f9e75ca0
FangWolf <siyu2510@163.com>
2018-04-15 09:38:33 +0800

    all are ok

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +29,37 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        Path path = new Path();
+        Paint paint = new Paint();
+        //画坐标系
+        path.moveTo(100,50);
+        path.lineTo(100, 600);
+        path.rLineTo(800, 0);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.WHITE);
+        canvas.drawPath(path, paint);
+        //画直条
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(120,598,220,600,paint);
+        canvas.drawRect(230,580,330,600,paint);
+        canvas.drawRect(340,580,440,600,paint);
+        canvas.drawRect(450,400,550,600,paint);
+        canvas.drawRect(560,300,660,600,paint);
+        canvas.drawRect(670,200,770,600,paint);
+        canvas.drawRect(780,450,880,600,paint);
+        //画字
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(30);
+        canvas.drawText("Froyo",130,630,paint);
+        canvas.drawText("GB",260,630,paint);
+        canvas.drawText("ICS",370,630,paint);
+        canvas.drawText("JB",480,630,paint);
+        canvas.drawText("KiKat",580,630,paint);
+        canvas.drawText("L",710,630,paint);
+        canvas.drawText("M",820,630,paint);
+        paint.setTextSize(50);
+        canvas.drawText("直方图",450,700,paint);
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +29,56 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        //画标注
+        Path path = new Path();
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setTextSize(30);
+        canvas.drawText("Lollipop",30,50,paint);
+        path.moveTo(150,50);
+        path.lineTo(250,50);
+        path.rLineTo(50,50);
+        canvas.drawPath(path,paint);
+        canvas.drawText("KitKat",50,500,paint);
+        path.moveTo(150,500);
+        path.lineTo(220,500);
+        path.rLineTo(50,-50);
+        canvas.drawPath(path,paint);
+        canvas.drawText("Marshmallow",750,150,paint);
+        path.moveTo(730,150);
+        path.lineTo(630,150);
+        path.rLineTo(-50,50);
+        canvas.drawPath(path,paint);
+        canvas.drawText("Gingerbread",750,330,paint);
+        path.moveTo(730,330);
+        path.lineTo(630,330);
+        canvas.drawPath(path,paint);
+        canvas.drawText("Ice Cream Sandwich",750,400,paint);
+        path.moveTo(730,400);
+        path.lineTo(700,400);
+        path.rLineTo(-30,-30);
+        path.rLineTo(-30,0);
+        canvas.drawPath(path,paint);
+        canvas.drawText("Jelly Bean",750,500,paint);
+        path.moveTo(730,500);
+        path.lineTo(600,500);
+        path.rLineTo(-50,-50);
+        canvas.drawPath(path,paint);
+        //画饼
+        paint.setAntiAlias(false);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        canvas.drawArc(140,40,640,540,-60,-120,true,paint);
+        paint.setColor(Color.YELLOW);
+        canvas.drawArc(150,50,650,550,0,-60,true,paint);
+        paint.setColor(Color.CYAN);
+        canvas.drawArc(150,50,650,550,3,7,true,paint);
+        paint.setColor(Color.GRAY);
+        canvas.drawArc(150,50,650,550,13,7,true,paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawArc(150,50,650,550,23,57,true,paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawArc(150,50,650,550,83,97,true,paint);
     }
 }
